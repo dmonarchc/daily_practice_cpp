75c1550f3ba07d64283a6034c13c63c51af3a803
dmonarchc <cisternasdelriodaniel@gmail.com>
2025-05-13 04:23:53 -0400

    Add book.cpp book.h and change on main.cpp

--- day05_struct_project/main.cpp
+++ day05_struct_project/main.cpp
@@ -1,37 +1,4 @@
-#include <iostream>
-#include <string>
-
-struct Book
-{
-    std::string title;
-    std::string author;
-    int year;
-    float price;
-};
-
-void print_book(const Book &book)
-{
-    std::cout << "-------------------------" << std::endl;
-    std::cout << "Título : " << book.title << std::endl;
-    std::cout << "Autor  : " << book.author << std::endl;
-    std::cout << "Año    : " << book.year << std::endl;
-    std::cout << "Precio : $" << book.price << std::endl;
-    std::cout << "-------------------------" << std::endl
-              << std::endl;
-}
-
-void insert_book(Book &book)
-{
-    std::cout << "Ingrese titulo: " << std::endl;
-    std::cout << "Ingrese titulo: " << std::endl;
-    std::getline(std::cin >> std::ws, book.title);
-    std::cout << "Ingrese autor: " << std::endl;
-    std::getline(std::cin >> std::ws, book.author);
-    std::cout << "Ingrese anio: " << std::endl;
-    std::cin >> book.year;
-    std::cout << "Ingrese precio: " << std::endl;
-    std::cin >> book.price;
-}
+#include "book.h"
 
 int main()
 {
